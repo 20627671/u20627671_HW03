ab585d55bf42a4a92063301322b0fb7669103bca
User <User@DESKTOP-3N8EBOR>
2023-10-29 16:11:04 +0200

    Add project files.

---
+++ Models/LibraryVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Homework03.Models
+{
+    public class LibraryVM
+    {
+
+        public IEnumerable<students> mystudentsList { get; set; }
+        public IEnumerable<types> mytypeList { get; set; }
+        public IEnumerable<borrows> borrowsList { get; set; }
+        public IEnumerable<books> booksList { get; set; }
+        [NotMapped]
+        public List<string> numOfBookstaken { get; set; }
+    }
+}
